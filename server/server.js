bb13d63f8b907624538ff4c2a85e12fc908fc111
prkr-28 <priyanshu402muz@gmail.com>
2026-05-27 19:24:29 +0530

    base urls fixed

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(helmet());
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'https://civicpulse-gn5g.onrender.com',
   credentials: true
 }));
 
